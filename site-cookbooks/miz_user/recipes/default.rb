3c7847a48167519e4091b9f92a6c458754d3c873
miz <mizugokoro@gmail.com>
2013-05-27 19:19:26 +0900

    added miz user definition

---
+++ site-cookbooks/miz_user/recipes/default.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: miz_user
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+keyfilename="authorized_keys"
+
+user "miz" do
+     action :remove
+end
+
+user "miz" do
+     home "/home/miz"
+     shell "/bin/zsh"
+     action [:create, :manage]
+end
+
+group "wheel" do
+     append true
+     members "miz"
+     action [:create,:manage]
+end
+
+home_dir = "/home/miz"
+ssh_dir = home_dir + "/.ssh"
+authorized_keys_file = ssh_dir + "/authorized_keys"
+
+directory ssh_dir do
+  owner "miz"
+  group "miz"
+  mode 0700
+end
+
+cookbook_file "#{authorized_keys_file}" do
+  source "#{keyfilename}"
+  owner "miz"
+  group "miz"
+  mode 0600
+end
